92de65277c0587e8d64e19ecd7415dc46c489f2d
Rohit Lalwani <rohit.l@chapter247.com>
2024-05-28 18:47:04 +0530

    (feat) : Setting up the docker

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -3,11 +3,11 @@ import userRoute from './user/user.route';
 import schoolRoute from './school/school.route';
 import testRoute from './testType/testType.route';
 import authRoute from './auth/auth.route';
-import { version } from '../../backend/package.json';
 import resourceRoute from './resource/resource.route';
 import profileRoute from './profile/profile.route';
 
 const router = express.Router();
+const version = process.env.APP_VERSION ?? '1.0.0';
 
 router.use(`/api/${version}/user`, userRoute);
 router.use(`/api/${version}/school`, schoolRoute);
